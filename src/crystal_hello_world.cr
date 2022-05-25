9d4c5cf0d3d347bde6c4e376a7025016f0330658
Billy.Zheng <vil963@gmail.com>
2022-05-26 00:45:27 +0800

    Add hello world page

--- src/crystal_hello_world.cr
+++ src/crystal_hello_world.cr
@@ -1,1 +1,12 @@
-puts "hello world!"
+require "kemal"
+
+get "/" do |env|
+  env.redirect "/world"
+end
+
+get "/:name" do |env|
+  name = env.params.url["name"]
+  render "src/views/index.ecr"
+end
+
+Kemal.run
