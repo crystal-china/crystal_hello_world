39bf3dcd8ce9cdf6f08ac4fe91f6a41ea30d8f19
Billy.Zheng <vil963@gmail.com>
2022-07-27 17:15:57 +0800

    Add kemal spec

--- spec/crystal_hello_world_spec.cr
+++ spec/crystal_hello_world_spec.cr
@@ -1,9 +1,8 @@
 require "./spec_helper"
 
-describe "CrystalHelloWorld" do
-  # TODO: Write tests
-
-  it "works" do
-    false.should eq(false)
+describe "Kemal::App" do
+  it "renders /world" do
+    get "/world"
+    response.body.should eq "<html>\n  <head>\n    <meta charset=\"utf-8\">\n    <script src=\"/materialize/js/materialize.js\"></script>\n    <link rel=\"stylesheet\" href=\"/materialize/css/materialize.min.css\"/>\n    <title>world</title>\n  </head>\n  <body>\n    <div class=\"container\">\n      <p>Hello, world!</p>\n    </div>\n  </body>\n</html>\n"
   end
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,2 @@
-require "spec"
+require "spec-kemal"
 require "../src/crystal_hello_world"
