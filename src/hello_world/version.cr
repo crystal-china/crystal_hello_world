56c09e4be2669966b46900fed710cbc1aeccbede
Billy.Zheng <vil963@gmail.com>
2025-03-19 01:46:09 +0800

    Try add date to VERSION

--- src/hello_world/version.cr
+++ src/hello_world/version.cr
@@ -1,3 +1,9 @@
 class HelloWorld
-  VERSION = {{ `shards version "#{__DIR__}"`.chomp.stringify }}
+  VERSION = {{
+              `shards version "#{__DIR__}"`.chomp.stringify +
+              " (rev " +
+              `git rev-parse --short HEAD`.chomp.stringify +
+              ")" +
+              `date '+ %Y-%m-%d %H:%M:%S'`.chomp.stringify
+            }}
 end
