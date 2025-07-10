99843d6a40d197d78a37607bc3fbae81aab3a245
Billy.Zheng <vil963@gmail.com>
2025-07-10 12:45:15 +0800

    Try typos action

--- spec/crystal_hello_world_spec.cr
+++ spec/crystal_hello_world_spec.cr
@@ -1,7 +1,7 @@
 require "./spec_helper"
 
 describe "Kemal::App" do
-  it "renders /world" do
+  it "render /world" do
     get "/world"
     response.body.should eq "<html>\n  <head>\n    <meta charset=\"utf-8\">\n    <script src=\"/materialize/js/materialize.js\"></script>\n    <link rel=\"stylesheet\" href=\"/materialize/css/materialize.min.css\"/>\n    <title>world</title>\n  </head>\n  <body>\n    <div class=\"container\">\n      <p>Hello, world!</p>\n    </div>\n  </body>\n</html>\n"
   end
